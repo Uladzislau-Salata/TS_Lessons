8c3493690228ed53f64617f0bdd58c9853f1a5f2
Uladzislau <salata.vlad131997@yandex.ru>
2025-11-09 14:04:28 +0300

    7. Базовые типы: строка, число, логическое значение

--- index.ts
+++ index.ts
@@ -1,2 +1,19 @@
-const str = "str";
-console.log(str);
+// let userName: string = "Ivan";
+
+// userName.isInteger();
+//
+// userName = "fgdfg";
+
+// '', "", ``
+// 10, 0.5, 0.001, -50, 4e10
+// true, false
+
+const isBirthday: boolean = true;
+let age: number;
+const userName: string = "John";
+
+age = 40;
+
+if (isBirthday) {
+  console.log(`Congrats ${userName.toLocaleUpperCase()}, age: ${age + 1}`);
+}
