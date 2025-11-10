bd5ea4ab948cd6d6d94c80ee2308ae326f72db39
Uladzislau <salata.vlad131997@yandex.ru>
2025-11-10 20:35:24 +0300

    9. Специальный тип any

--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
-const isBirthdayData: boolean = true;
-let ageData: number = 40;
-const userNameData: string = "John";
+// const isBirthdayData: boolean = true;
+// let ageData: number = 40;
+// const userNameData: string = "John";
 
-// function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
-//   if (isBirthday) {
-//     return `Congrats ${userName.toLocaleUpperCase()}, age: ${age + 1}`;
-//   } else {
-//     return "Error";
-//   }
-// }
+let salary: number;
+salary = 5000;
 
-const logBrtMsg = (
-  isBirthday: boolean,
-  userName: string,
-  age: number
-): string => {
+const userData =
+  '{ "isBirthdayData": true, "ageData": 40, "userNameData": "John" }';
+
+const userObj: {
+  isBirthdayData: boolean;
+  userNameData: string;
+  ageData: number;
+} = JSON.parse(userData);
+
+function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
   if (isBirthday) {
     return `Congrats ${userName.toLocaleUpperCase()}, age: ${age + 1}`;
   } else {
     return "Error";
   }
-};
+}
 
 logBrtMsg(isBirthdayData, userNameData, ageData);
