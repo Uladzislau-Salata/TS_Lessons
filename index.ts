791fb2c1c122ad76b385248cf65cec45d941770c
Uladzislau <salata.vlad131997@yandex.ru>
2025-11-14 18:52:17 +0300

    17. Tuples (Кортежи)

--- index.ts
+++ index.ts
@@ -1,50 +1,39 @@
-const electricityUserData = {
-  readings: 95,
-  units: "kWt",
-  mode: "double",
+const userData = {
+  isBirthdayData: true,
+  ageData: 40,
+  userNameData: "John",
+  messages: { error: "Error" },
 };
 
-const waterUserData = {
-  readings: 3,
-  units: "m3",
+const userDataTuple: [...boolean[], number, string] = [true, false, 40, "John"];
+// userDataTuole[3];
+// userDataTuole.push(50);
+// userDataTuole[3];
+
+const res = userDataTuple.map((t) => `${t}-data`);
+
+const [bthd, age, userName] = userDataTuple;
+
+const createError = (msg: string) => {
+  throw new Error(msg);
 };
 
-const elRate: number = 0.45;
-const wRate: number = 2;
-
-const monthPayments: number[] = [0, 0]; // [electricity, water]
-
-const calculatePayments = (
-  { readings, mode }: { readings: number; mode: string },
-  wData: { readings: number },
-  elRate: number,
-  wRate: number
-): void => {
-  if (mode === "double" && readings < 50) {
-    monthPayments[0] = readings * elRate * 0.7;
+function logBrtMsg({
+  isBirthdayData,
+  userNameData,
+  ageData,
+  messages: { error },
+}: {
+  isBirthdayData: boolean;
+  userNameData: string;
+  ageData: number;
+  messages: { error: string };
+}): string {
+  if (isBirthdayData) {
+    return `Congrats ${userNameData.toLocaleUpperCase()}, age: ${ageData + 1}`;
   } else {
-    monthPayments[0] = readings * elRate;
+    return createError(error);
   }
+}
 
-  monthPayments[1] = wData.readings * wRate;
-};
-
-calculatePayments(electricityUserData, waterUserData, elRate, wRate);
-
-const sendInvoice = (
-  [el, water]: number[],
-  electricityUserData: { readings: number; units: string },
-  waterUserData: { readings: number; units: string }
-): string => {
-  const text = `    Hello!
-    This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
-    It will cost: ${el}€
-    
-    This month you used ${waterUserData.readings} ${waterUserData.units} of water
-    It will cost: ${water}€`;
-
-  return text;
-};
-
-const invoice = sendInvoice(monthPayments, electricityUserData, waterUserData);
-console.log(invoice);
+console.log(logBrtMsg(userData));
