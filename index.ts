5b39d55c695e5a9687b44ad1a09c97b50c083a2c
Uladzislau <salata.vlad131997@yandex.ru>
2025-11-23 13:23:05 +0300

    30. Модификаторы свойств: readonly (Property Modifiers)

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 interface User {
-  login: string;
+  readonly login: string;
   password: string;
   age: number;
   // addr?: string;
-  addr: string | undefined;
+  readonly addr: string | undefined;
   parents?: {
     mother?: string;
     father?: string;
@@ -17,11 +17,34 @@ const user: User = {
   addr: "qwdfsd",
 };
 
-let dbName: string;
-sendUserDate(user, "rtrgrfds");
-console.log(dbName!);
+const userFreeze: Readonly<User> = {
+  login: "first",
+  password: "qwerty",
+  age: 50,
+  addr: "qwdfsd",
+};
+
+userFreeze.age = 3345;
+userFreeze.password = "123123";
+
+// user.login = "asdvf";
+user.password = "asdvf";
+
+let dbName = "12345";
 
 function sendUserDate(obj: User, db?: string): void {
   dbName = "1234";
-  console.log(obj.parents!.mother?.toLowerCase(), db!.toLowerCase);
+  console.log(obj.parents?.mother?.toLowerCase(), db?.toLowerCase);
 }
+
+const baasicPorts: ReadonlyArray<number> = [3000, 3001, 5555];
+baasicPorts[0] = 5;
+baasicPorts.push(4440);
+
+// const baasicPorts: readonly number[] = [3000, 3001, 5555];
+// baasicPorts[0] = 5;
+// baasicPorts.push(4440);
+
+// const baasicPorts: readonly [number, ...string[]] = [3000, "3001", "5555"];
+// baasicPorts[0] = 5;
+// baasicPorts.push(4440);
