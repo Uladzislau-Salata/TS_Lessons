0ca25665f0aa4a0cffed194133fe712b70d82f0e
Uladzislau <salata.vlad131997@yandex.ru>
2025-11-23 12:31:14 +0300

    27. Механизм вывода типов (Type Inference)

--- index.ts
+++ index.ts
@@ -1,82 +1,22 @@
-type ValidAmount = "empty" | number;
+const salary = 500;
 
-// структура данных склада с одеждой
-
-interface IClothesWarehouse {
-  jackets: ValidAmount;
-  hats: ValidAmount;
-  socks: ValidAmount;
-  pants: ValidAmount;
+interface UserData {
+  isBirthdayData: boolean;
+  ageData: number;
+  userNameData: string;
 }
 
-// структура данных склада с канцтоварами
+const userData = '{"isBirthdayData":true,"ageData":40, "userNameData":"John"}';
 
-interface IStationeryWarehouse {
-  scissors: ValidAmount;
-  paper: "empty" | boolean;
+const arr = ["sss", 5, true];
+
+const userObj: UserData = JSON.parse(userData);
+
+console.log(userObj.smt);
+
+const isOkay = true;
+let movement: boolean | string = false;
+
+if (isOkay) {
+  movement = "moving";
 }
-
-// структура данных склада с бытовой техникой
-
-interface IAppliancesWarehouse {
-  dishwashers: ValidAmount;
-  cookers: ValidAmount;
-  mixers: ValidAmount;
-}
-
-// общая структура данных, наследует все данные из трех выше
-// + добавляет свои
-
-interface ITotalWarehouse
-  extends IClothesWarehouse,
-    IStationeryWarehouse,
-    IAppliancesWarehouse {
-  deficit: boolean;
-  date: Date;
-}
-
-// главный объект со всеми данными, должен подходить под формат TotalWarehouse
-
-const totalData: ITotalWarehouse = {
-  jackets: 5,
-  hats: "empty",
-  socks: "empty",
-  pants: 15,
-  scissors: 15,
-  paper: true,
-  dishwashers: 3,
-  cookers: "empty",
-  mixers: 14,
-  deficit: true,
-  date: new Date(),
-};
-
-// Реализуйте функцию, которая принимает в себя главный объект totalData нужного формата
-// и возвращает всегда строку
-// Функция должна отфильтровать данные из объекта и оставить только те названия товаров, у которых значение "empty"
-// и поместить их в эту строку. Если таких товаров нет - возвращается другая строка (см ниже)
-
-// С данным объектом totalData строка будет выглядеть:
-// "We need this items: hats, socks, cookers"
-// Товары через запятую, в конце её не должно быть. Пробел после двоеточия, в конце строки его нет.
-
-function printReport(data: ITotalWarehouse) {
-  const result: string = Object.entries(data)
-    .filter((item) => {
-      return item[1] === "empty";
-    })
-    .map((arr) => {
-      return arr[0];
-    })
-    .join(", ");
-
-  console.log(result);
-
-  if (result === "") {
-    return "Everything fine";
-  } else {
-    return `We need this items: ${result}`;
-  }
-}
-
-console.log(printReport(totalData));
