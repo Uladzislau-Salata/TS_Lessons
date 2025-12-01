85970b943b00569974052d3e4f51e50bb11ac428
Uladzislau <salata.vlad131997@yandex.ru>
2025-12-01 23:12:36 +0300

    36. Немного про “внутренние” типы и приведение типов

--- index.ts
+++ index.ts
@@ -1,36 +1,39 @@
-const fetchData = (url: string, method: "GET" | "POST"): void => {
-  console.log(method);
+// let num: Number = new Number(5);
+// let num2: number = 5;
+// let num3 = Number(5);
+
+// num=num2;
+// num2=num;
+
+const num = 5;
+
+const strNum: string = num.toString();
+
+const str = "5";
+const numStr: number = +str;
+
+interface Depatment {
+  name: string;
+  budget: number;
+}
+
+const depatment: Depatment = {
+  name: "web-dev",
+  budget: 500,
 };
 
-const reqOption = {
-  url: "https://someurl.com",
-  method: "GET",
-} as const;
+interface Project {
+  name: string;
+  projectBudget: number;
+}
 
-const str = "str";
-const method = "GET";
+function transfromDepartment(depatment: Depatment, amount: number): Project {
+  return {
+    name: depatment.name,
+    projectBudget: amount,
+  };
+}
 
-fetchData("qqq", "GET");
-// fetchData(reqOption.url, reqOption.method as "GET");
-fetchData(reqOption.url, <"GET">reqOption.method);
+const mainProject: Project = transfromDepartment(depatment, 4400);
 
-const box = document.querySelector(".box") as HTMLElement;
-const input = <HTMLInputElement>document.querySelector("input");
-
-// const someNumber: number = input.value as any as number;
-const someNumber: number = +input.value;
-console.log(someNumber);
-// box.style;
-// box?.classList;
-let a = "value" as const;
-
-let b = { f: 100 } as const;
-let c = [] as const;
-
-let value = "value";
-let arr = ["sd", "dff"];
-let obj = { f: 100 };
-
-// let T0 = value as const;
-
-// let T5 =(Math.round(Math.random()*1)?'yes':'no')as const;
+console.log(mainProject);
