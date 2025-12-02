d06a97e0fb14cdcae5d7dc29ea482c62348d72b2
Uladzislau <salata.vlad131997@yandex.ru>
2025-12-02 22:16:08 +0300

    38. Кейс использования never

--- index.ts
+++ index.ts
@@ -20,32 +20,77 @@ function isNumber(n: unknown): n is number {
 }
 
 interface Car {
+  name: "car";
   engine: string;
-  wheels: {
-    number: number;
-    type: string;
-  };
+  wheels: number;
 }
 
 interface Ship {
+  name: "ship";
   engine: string;
   sail: string;
 }
 
-function repairVehicle(vehicle: Car | Ship) {
-  if (isCar(vehicle)) {
-    vehicle.wheels;
-  } else if (isShip(vehicle)) {
-    vehicle;
-  } else {
-    vehicle;
-  }
+interface Airplane {
+  name: "airplane";
+  engine: string;
+  wings: string;
 }
 
-function isCar(car: Car | Ship): car is Car {
-  return (car as Car).wheels.number !== undefined;
+interface SuperAirplane {
+  name: "smth";
+  engine: string;
+  wings: string;
 }
 
-function isShip(ship: Car | Ship): ship is Ship {
+type Vehicle = Car | Ship | Airplane | SuperAirplane;
+
+function isCar(car: Vehicle): car is Car {
+  return "wheels" in car;
+}
+
+function isShip(ship: Vehicle): ship is Ship {
   return "sail" in ship;
 }
+
+function repairVehicle(vehicle: Vehicle) {
+  switch (vehicle.name) {
+    case "car":
+      console.log(vehicle.wheels);
+      break;
+    case "ship":
+      console.log(vehicle.sail);
+      break;
+    case "airplane":
+      console.log(vehicle.wings);
+      break;
+    case "smth":
+      console.log(vehicle.wings);
+      break;
+
+    default:
+      const smth: never = vehicle;
+      console.log("Ouuups!");
+  }
+
+  // if (isCar(vehicle)) {
+  //   vehicle.wheels;
+  // } else if (isShip(vehicle)) {
+  //   vehicle;
+  // } else {
+  //   vehicle.wings;
+  // }
+}
+
+// const logBrtMsg = (
+//   isBirthday: boolean,
+//   userName: string,
+//   age: number
+// ): string => {
+//   if (isBirthday === true) {
+//     return `Congrats${userName}, age:${age + 1}`;
+//   } else if (isBirthday === false) {
+//     return "Without presents today";
+//   }
+//   return createError("Error");
+// };
