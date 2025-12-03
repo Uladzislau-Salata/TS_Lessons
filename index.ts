912054c73e55c950fa7ea04efcc291cccfb9d16c
Uladzislau <salata.vlad131997@yandex.ru>
2025-12-03 21:38:08 +0300

    40. Разделение интерфейсов на правильные части

--- index.ts
+++ index.ts
@@ -1,38 +1,60 @@
-interface Square {
-  side: number;
-  area: number;
+interface Car {
+  name: "car";
+  engine: string;
+  wheels: number;
 }
 
-interface Rect {
-  a: number;
-  b: number;
-  area: number;
+interface Ship {
+  name: "ship";
+  engine: string;
+  sail: string;
 }
 
-function calculateArea(side: number): Square;
-function calculateArea(a: number, b: number, c: number, d: number): Rect;
+interface Airplane {
+  name: "airplane";
+  engine: string;
+  wings: string;
+}
 
-function calculateArea(
-  a: number,
-  b?: number,
-  c?: number,
-  d?: number
-): Square | Rect {
-  if (b) {
-    const rect: Rect = {
-      a,
-      b,
-      area: a * b,
-    };
-    return rect;
-  } else {
-    const square: Square = {
-      side: a,
-      area: a * a,
-    };
-    return square;
+interface ComplexVehicle {
+  name: "car" | "ship" | "airplane";
+  engine: string;
+  wheels?: number;
+  sail?: string;
+  wings?: string;
+}
+
+type Vehicle = Car | Ship | Airplane;
+
+function isCar(car: Vehicle): car is Car {
+  return "wheels" in car;
+}
+
+function isShip(ship: Vehicle): ship is Ship {
+  return "sail" in ship;
+}
+
+const car: ComplexVehicle = {
+  name: "car",
+  engine: "V8",
+};
+
+function repairVehicle(vehicle: ComplexVehicle) {
+  switch (vehicle.name) {
+    case "car":
+      console.log(vehicle.wheels! * 2);
+      break;
+    case "ship":
+      console.log(vehicle.sail);
+      break;
+    case "airplane":
+      console.log(vehicle.wings);
+      break;
+
+    default:
+      // const smth: never = vehicle;
+      console.log("Ouuups!");
   }
 }
 
-calculateArea(1);
-calculateArea(1, 5, 1, 5);
+repairVehicle(car);
