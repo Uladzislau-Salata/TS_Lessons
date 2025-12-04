0c2bab13e02f48784911cc2581332fc74a5e315f
Uladzislau <salata.vlad131997@yandex.ru>
2025-12-04 20:28:32 +0300

    41. Практика создания type guard и правильных интерфейсов (2)

--- index.ts
+++ index.ts
@@ -9,21 +9,28 @@ interface res1Data extends infoAnimal {
   age?: number;
 }
 
+enum AnimalStatus {
+  Available = "available",
+  NotAvailable = "not available",
+}
+
 interface response1 {
-  status: "available";
+  status: AnimalStatus.Available;
   data: res1Data;
 }
 
+interface AnimalNotAvailableData {
+  message: string;
+  nextUpdateIn: Date;
+}
+
 interface response2 {
-  status: "not available";
-  data: {
-    message: string;
-    nextUpdateIn: Date;
-  };
+  status: AnimalStatus.NotAvailable;
+  data: AnimalNotAvailableData;
 }
 
 function checkAnimalData(animal: response1 | response2): res1Data | string {
-  if (animal.status === "available") {
+  if (animal.status === AnimalStatus.Available) {
     return animal.data;
   } else {
     return `${animal.data.message}, you can try in ${animal.data.nextUpdateIn}`;
@@ -31,7 +38,7 @@ function checkAnimalData(animal: response1 | response2): res1Data | string {
 }
 
 const res1: response1 = {
-  status: "available",
+  status: AnimalStatus.Available,
   data: {
     animal: "cat",
     breed: "корги",
@@ -42,7 +49,7 @@ const res1: response1 = {
 };
 
 const res2: response2 = {
-  status: "not available",
+  status: AnimalStatus.NotAvailable,
   data: {
     message: "Sorry, no dogs were found matching your criteria.",
     nextUpdateIn: new Date(2025, 11, 12),
@@ -52,58 +59,58 @@ const res2: response2 = {
 console.log(checkAnimalData(res1));
 console.log(checkAnimalData(res2));
 
-type Animal = "cat" | "dog" | "bird";
+// type Animal = "cat" | "dog" | "bird";
 
-// Можно и не создавать перечисление, но вдруг в будущем статусов будет больше?
+// // Можно и не создавать перечисление, но вдруг в будущем статусов будет больше?
 
-enum AnimalStatus {
-  Available = "available",
-  NotAvailable = "not available",
-}
+// enum AnimalStatus {
+//   Available = "available",
+//   NotAvailable = "not available",
+// }
 
-interface AnimalData {
-  animal: Animal;
-  breed: string;
-  sterilized?: string;
-}
-// Не повторяем код, используем extends
-interface AnimalAvailableData extends AnimalData {
-  location: string;
-  age?: number;
-}
+// interface AnimalData {
+//   animal: Animal;
+//   breed: string;
+//   sterilized?: string;
+// }
+// // Не повторяем код, используем extends
+// interface AnimalAvailableData extends AnimalData {
+//   location: string;
+//   age?: number;
+// }
 
-interface AnimalNotAvailableData {
-  message: string;
-  nextUpdateIn: Date;
-}
+// interface AnimalNotAvailableData {
+//   message: string;
+//   nextUpdateIn: Date;
+// }
 
-// Интерфейсы стоит разделить, так как оба ответа будут иметь поле data
-// И только по статусу будет сложно определить данные
+// // Интерфейсы стоит разделить, так как оба ответа будут иметь поле data
+// // И только по статусу будет сложно определить данные
 
-interface AnimalAvailableResponse {
-  status: AnimalStatus.Available;
-  data: AnimalAvailableData;
-}
+// interface AnimalAvailableResponse {
+//   status: AnimalStatus.Available;
+//   data: AnimalAvailableData;
+// }
 
-interface AnimalNotAvailableResponse {
-  status: AnimalStatus.NotAvailable;
-  data: AnimalNotAvailableData;
-}
+// interface AnimalNotAvailableResponse {
+//   status: AnimalStatus.NotAvailable;
+//   data: AnimalNotAvailableData;
+// }
 
-type Res = AnimalAvailableResponse | AnimalNotAvailableResponse;
+// type Res = AnimalAvailableResponse | AnimalNotAvailableResponse;
 
-function isAvailable(res: Res): res is AnimalAvailableResponse {
-  if (res.status === AnimalStatus.Available) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// function isAvailable(res: Res): res is AnimalAvailableResponse {
+//   if (res.status === AnimalStatus.Available) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-function checkAnimalData(animal: Res): AnimalAvailableData | string {
-  if (isAvailable(animal)) {
-    return animal.data;
-  } else {
-    return `${animal.data.message}, you can try in ${animal.data.nextUpdateIn}`;
-  }
-}
+// function checkAnimalData(animal: Res): AnimalAvailableData | string {
+//   if (isAvailable(animal)) {
+//     return animal.data;
+//   } else {
+//     return `${animal.data.message}, you can try in ${animal.data.nextUpdateIn}`;
+//   }
+// }
