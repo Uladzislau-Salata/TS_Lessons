5e43ccd1a462755c562bc2540785085967bfd313
Uladzislau <salata.vlad131997@yandex.ru>
2025-12-04 20:49:13 +0300

    42. Работа с DOM

--- index.ts
+++ index.ts
@@ -1,116 +1,19 @@
-interface infoAnimal {
-  animal: "cat" | "dog" | "bird";
-  breed: string;
-  sterilized?: string;
+const box = document.querySelector(".box") as HTMLElement;
+
+const input = document.querySelector("input");
+input?.value;
+
+const link = document.querySelector("a");
+if (link) {
+  link.href = "ggoygo";
 }
 
-interface res1Data extends infoAnimal {
-  location: string;
-  age?: number;
-}
+const p = document.querySelector(".paragraph") as HTMLParagraphElement;
 
-enum AnimalStatus {
-  Available = "available",
-  NotAvailable = "not available",
-}
+const links = document.querySelectorAll(".a");
 
-interface response1 {
-  status: AnimalStatus.Available;
-  data: res1Data;
-}
+const elem = document.createElement("a");
 
-interface AnimalNotAvailableData {
-  message: string;
-  nextUpdateIn: Date;
-}
-
-interface response2 {
-  status: AnimalStatus.NotAvailable;
-  data: AnimalNotAvailableData;
-}
-
-function checkAnimalData(animal: response1 | response2): res1Data | string {
-  if (animal.status === AnimalStatus.Available) {
-    return animal.data;
-  } else {
-    return `${animal.data.message}, you can try in ${animal.data.nextUpdateIn}`;
-  }
-}
-
-const res1: response1 = {
-  status: AnimalStatus.Available,
-  data: {
-    animal: "cat",
-    breed: "корги",
-    sterilized: "да",
-    location: "Гродно",
-    age: 5,
-  },
-};
-
-const res2: response2 = {
-  status: AnimalStatus.NotAvailable,
-  data: {
-    message: "Sorry, no dogs were found matching your criteria.",
-    nextUpdateIn: new Date(2025, 11, 12),
-  },
-};
-
-console.log(checkAnimalData(res1));
-console.log(checkAnimalData(res2));
-
-// type Animal = "cat" | "dog" | "bird";
-
-// // Можно и не создавать перечисление, но вдруг в будущем статусов будет больше?
-
-// enum AnimalStatus {
-//   Available = "available",
-//   NotAvailable = "not available",
-// }
-
-// interface AnimalData {
-//   animal: Animal;
-//   breed: string;
-//   sterilized?: string;
-// }
-// // Не повторяем код, используем extends
-// interface AnimalAvailableData extends AnimalData {
-//   location: string;
-//   age?: number;
-// }
-
-// interface AnimalNotAvailableData {
-//   message: string;
-//   nextUpdateIn: Date;
-// }
-
-// // Интерфейсы стоит разделить, так как оба ответа будут иметь поле data
-// // И только по статусу будет сложно определить данные
-
-// interface AnimalAvailableResponse {
-//   status: AnimalStatus.Available;
-//   data: AnimalAvailableData;
-// }
-
-// interface AnimalNotAvailableResponse {
-//   status: AnimalStatus.NotAvailable;
-//   data: AnimalNotAvailableData;
-// }
-
-// type Res = AnimalAvailableResponse | AnimalNotAvailableResponse;
-
-// function isAvailable(res: Res): res is AnimalAvailableResponse {
-//   if (res.status === AnimalStatus.Available) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
-// function checkAnimalData(animal: Res): AnimalAvailableData | string {
-//   if (isAvailable(animal)) {
-//     return animal.data;
-//   } else {
-//     return `${animal.data.message}, you can try in ${animal.data.nextUpdateIn}`;
-//   }
-// }
+link?.addEventListener("click", (e) => {
+  e.preventDefault();
+});
