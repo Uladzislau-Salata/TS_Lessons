a5f867a83ad1c2559ff3e80d4db60d450d9d896c
Uladzislau <salata.vlad131997@yandex.ru>
2025-12-14 20:42:10 +0300

    44. (д) Использование void внутри TS

--- index.ts
+++ index.ts
@@ -1,58 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const forms = document.querySelectorAll("form");
-  const email = document.querySelector("#email") as HTMLInputElement;
-  const title = document.querySelector("#title") as HTMLInputElement;
-  const text = document.querySelector("#text") as HTMLTextAreaElement;
-  const checkbox = document.querySelector("#checkbox") as HTMLInputElement;
+type voidFunc = () => void;
 
-  interface IFormData {
-    email: string;
-    title: string;
-    text: string;
-    checkbox: boolean;
-  }
+const retString: voidFunc = () => {
+  //... some magic
+  return "string";
+};
 
-  const formData: IFormData = {
-    email: "",
-    title: "",
-    text: "",
-    checkbox: false,
-  };
+const s = retString();
+console.log(s);
 
-  forms.forEach((form) =>
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
+const retNum: voidFunc = () => {
+  //... some magic
+  return 5;
+};
 
-      // Можно и создавать другой объект для соблюдения иммутабельности, но пока не обязательно
-      formData.email = email?.value ?? "";
-      formData.title = title?.value ?? "";
-      formData.text = text?.value ?? "";
-      formData.checkbox = checkbox?.checked ?? false;
+const n = retNum();
 
-      if (validateFormData(formData)) {
-        checkFormData(formData);
-      }
-    })
-  );
+console.log(n);
 
-  function validateFormData(data: IFormData): boolean {
-    // Если каждое из свойств объекта правдиво...
-    if (Object.values(data).every((value) => value)) {
-      return true;
-    } else {
-      console.log("Please, complete all fields");
-      return false;
-    }
-  }
+function f2(): void {
+  return true;
+}
 
-  function checkFormData(data: IFormData) {
-    const { email } = data;
-    const emails = ["example@gmail.com", "example@ex.com", "admin@gmail.com"];
+const f3 = function (): void {
+  return true;
+};
 
-    if (emails.some((e) => e === email)) {
-      console.log("This email is already exist");
-    } else {
-      console.log("Posting data...");
-    }
-  }
-});
+const names = ["Anna", "John"];
+const newArr = names.slice();
+
+names.forEach((name, i, arr) => arr.push("Hey"));
