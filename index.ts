c462b54e1c6813496b3cb9462110ed3822b19e7b
Uladzislau <salata.vlad131997@yandex.ru>
2026-01-19 21:24:06 +0300

    50. Generics classes

--- index.ts
+++ index.ts
@@ -1,127 +1,30 @@
-// Создать Generic-интерфейс PlayerData, который подходил бы для создания таких объектов:
+class User<T, S> {
+  name: T;
+  age: S;
 
-interface PlayerData<Game, Hours> {
-  game: Game;
-  hours: Hours;
-  server: string;
-}
+  constructor(name: T, age: S) {
+    this.name = name;
+    this.age = age;
+  }
 
-const player1: PlayerData<string, number> = {
-  game: "CS:GO",
-  hours: 300,
-  server: "basic",
-};
-
-const player2: PlayerData<number, string> = {
-  game: 2048,
-  hours: "300 h.",
-  server: "arcade",
-};
-
-const player3: PlayerData<string, object> = {
-  game: "Chess",
-  hours: {
-    total: 500,
-    inMenu: 50,
-  },
-  server: "chess",
-};
-
-// Массив данных с фигурами содержит объекты, у каждого из которых обязательно есть свойство name
-// Каждый объект может еще содержать дополнительные свойства в случайном виде
-// Свойство name может иметь только 4 варианта
-// Функция calculateAmountOfFigures должна принимать массив с объектами, у которых обязательно должно быть свойство name
-// Возвращает она объект-экземпляр AmountOfFigures
-// Внутри себя подсчитывает сколько каких фигур было в массиве и записывает результаты в AmountOfFigures
-// С текущими данными в консоль должно попадать:
-// { squares: 3, circles: 2, triangles: 2, others: 1 }
-
-interface AmountOfFigures {
-  squares: number;
-  circles: number;
-  triangles: number;
-  others: number;
-}
-
-enum FigureNames {
-  RECT = "rect",
-  TRIANGLE = "triangle",
-  LINE = "line",
-  CIRCLE = "circle",
-}
-
-interface Figure {
-  name: FigureNames;
-}
-
-interface CustomFigure extends Figure {
-  data?: Record<string, number>;
-}
-
-function calculateAmountOfFigures<T extends Figure>(
-  figure: T[],
-): AmountOfFigures {
-  let amount: AmountOfFigures = {
-    squares: 0,
-    circles: 0,
-    triangles: 0,
-    others: 0,
-  };
-
-  figure.forEach((fig) => {
-    switch (fig.name) {
-      case FigureNames.RECT:
-        amount.squares++;
-        break;
-      case FigureNames.TRIANGLE:
-        amount.triangles++;
-        break;
-      case FigureNames.LINE:
-        amount.others++;
-        break;
-      case FigureNames.CIRCLE:
-        amount.circles++;
-        break;
-      default:
-        break;
+  sayMyFullName<T>(surname: T): string {
+    if (typeof surname !== "string") {
+      return `I have only name: ${this.name}`;
+    } else {
+      return `${this.name} ${surname}`;
     }
-  });
-
-  return amount;
+  }
 }
 
-const data: CustomFigure[] = [
-  {
-    name: FigureNames.RECT,
-    data: { a: 5, b: 10 },
-  },
-  {
-    name: FigureNames.RECT,
-    data: { a: 6, b: 11 },
-  },
-  {
-    name: FigureNames.TRIANGLE,
-    data: { a: 5, b: 10, c: 14 },
-  },
-  {
-    name: FigureNames.LINE,
-    data: { l: 15 },
-  },
-  {
-    name: FigureNames.CIRCLE,
-    data: { r: 10 },
-  },
-  {
-    name: FigureNames.CIRCLE,
-    data: { r: 5 },
-  },
-  {
-    name: FigureNames.RECT,
-    data: { a: 15, b: 7 },
-  },
-  {
-    name: FigureNames.TRIANGLE,
-  },
-];
+class AdminUser<T> extends User<string, number> {
+  rules: T;
+}
 
-console.log(calculateAmountOfFigures(data));
+const ivan = new User("Ivan", 30);
+
+console.log(ivan.sayMyFullName("Smith"));
+
+const nameData = "Alex";
+const ageData = 54;
+
+const alex = new User<string, number>(nameData, ageData);
