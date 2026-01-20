faacfed41d3883f944bbf4c6f6549aec63f259f9
Uladzislau <salata.vlad131997@yandex.ru>
2026-01-20 21:56:29 +0300

    55. Indexed Access Types

--- index.ts
+++ index.ts
@@ -1,10 +1,29 @@
 interface Icompany {
   name: string;
   debts: number;
+  departments: Department[];
+  management: {
+    owner: string;
+  };
 }
 
+interface Department {
+  [key: string]: string;
+}
+
+// let debts = "debts" as "debts";
+// let debts: "debts" = "debts";
+const debts= "debts";
+
+type CompanyDebtsType = Icompany[typeof debts];
+// type CompanyDebtsType = Icompany["debts"];
+type CompanyOwnerType = Icompany["management"]["owner"];
+type CompanyDepartmentsType = Icompany["departments"][number];
+type CompanyDepartmentsTypes = Icompany["departments"];
+type Test = Icompany[keyof Icompany];
+
 type CompanyKeys = keyof Icompany;
-const keys: CompanyKeys = "name";
+const keys: CompanyKeys = "debts";
 
 function printDebts<T, K extends keyof T, S extends keyof T>(
   company: T,
@@ -17,10 +36,16 @@ function printDebts<T, K extends keyof T, S extends keyof T>(
 const google: Icompany = {
   name: "Google",
   debts: 500000,
+  departments: {
+    sales: "sales",
+    developer: "dev",
+  },
+  management: {
+    owner: "John",
+  },
 };
 
 printDebts(google, "name", "debts");
 
 type GoogleKeys = keyof typeof google;
-
 const keys2: GoogleKeys = "debts";
