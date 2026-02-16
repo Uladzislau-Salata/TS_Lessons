b9b63ff72d73966ca02e055184f5fdfa2ee8130f
Uladzislau <salata.vlad131997@yandex.ru>
2026-02-16 20:34:23 +0300

    78. Абстрактные классы

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Player {
-    #login;
-    _password;
-    server;
-    consent;
-    get password() {
-        return this._password;
-    }
-    set password(newPass) {
-        //Validation
-        this._password = newPass;
+class AbstractVehicle {
+    maodel;
+    capacity;
+    stopEngine(time) {
+        this.startEngine(new Date());
+        return "Engine Stopped";
     }
 }
-class CompetitivePlayer extends Player {
-    rank;
-    isConsented() {
-        this.consent ? "Yes" : "No";
-    }
+class Vehicle extends AbstractVehicle {
+    startEngine = (time) => {
+        return "Started";
+    };
 }
-const player = new Player();
-// player.#login
-// player.login = "asdfsdfad";
-// class User {
-//   public email!: string;
-//   public name!: string;
-//   constructor(email: string, name: string) {
-//     this.email = email;
-//     this.name = name;
-//   }
-// }
-// class User {
-//   constructor(
-//     public email: string,
-//     public name: string,
-//   ) {}
-// }
+// new Vehicle().
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,92 +1,23 @@
-function setName() {
-  return "COD";
+interface IEngine {
+  maodel: string;
+  capacity: number;
+  startEngine: (time: Date) => string;
 }
 
-class Player {
-  private static game: string;
-
-  #login!: string;
-  private _password!: string;
-  public server!: string;
-  protected consent!: boolean;
-
-  constructor(login: string) {
-    this.#login = login;
+abstract class AbstractVehicle {
+  maodel!: string;
+  capacity!: number;
+  abstract startEngine: (time: Date) => string;
+  stopEngine(time: string): string {
+    this.startEngine(new Date());
+    return "Engine Stopped";
   }
+}
 
-  static {
-    Player.game = setName();
-  }
-
-  get password() {
-    return this._password;
-  }
-
-  set password(newPass: string) {
-    //Validation
-    this._password = newPass;
-  }
-
-  static getGameName() {
-    return Player.game;
-  }
-
-  logIn = () => {
-    return `Player ${this.#login} online!`;
+class Vehicle extends AbstractVehicle {
+  startEngine = (time: Date) => {
+    return "Started";
   };
-  connect() {
-    //do smth
-    return this;
-  }
-
-  isPro(): this is CompetitivePlayer {
-    return this instanceof CompetitivePlayer;
-  }
 }
 
-// const test = player.logIn;
-// test();
-
-// new Player();
-// new Player();
-// new Player();
-// console.log(Player.getGameName());
-
-class CompetitivePlayer extends Player {
-  rank!: number;
-  checkLogin() {
-    return this.logIn();
-  }
-
-  private isConsented() {
-    this.consent ? "Yes" : "No";
-  }
-}
-
-const player = new Player("test");
-console.log(player.connect().logIn());
-
-const player2 = new CompetitivePlayer("Test2");
-console.log(player2.connect().logIn());
-
-const somePlayer: Player | CompetitivePlayer = new CompetitivePlayer("Test3");
-somePlayer.isPro() ? console.log(somePlayer) : console.log(somePlayer);
-
-// player.login = "asdfsdfad";
-
-// class User {
-//   public email!: string;
-//   public name!: string;
-
-//   constructor(email: string, name: string) {
-//     this.email = email;
-//     this.name = name;
-//   }
-// }
-
-// class User {
-//   constructor(
-//     public email: string,
-//     public name: string,
-//   ) {}
-// }
+// new Vehicle().
