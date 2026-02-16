3c700c86c38e121fd7edbe90738ab8584e68d067
Uladzislau <salata.vlad131997@yandex.ru>
2026-02-16 20:16:22 +0300

    77. this и типизация контекста

--- index.ts
+++ index.ts
@@ -10,13 +10,13 @@ class Player {
   public server!: string;
   protected consent!: boolean;
 
+  constructor(login: string) {
+    this.#login = login;
+  }
+
   static {
     Player.game = setName();
   }
-
-  //   constructor(game: string) {
-  //     Player.game = game;
-  //   }
 
   get password() {
     return this._password;
@@ -30,22 +30,47 @@ class Player {
   static getGameName() {
     return Player.game;
   }
+
+  logIn = () => {
+    return `Player ${this.#login} online!`;
+  };
+  connect() {
+    //do smth
+    return this;
+  }
+
+  isPro(): this is CompetitivePlayer {
+    return this instanceof CompetitivePlayer;
+  }
 }
 
-new Player();
-new Player();
-new Player();
-console.log(Player.getGameName());
+// const test = player.logIn;
+// test();
 
-// class CompetitivePlayer extends Player {
-//   rank!: number;
-//   private isConsented() {
-//     this.consent ? "Yes" : "No";
-//   }
-// }
+// new Player();
+// new Player();
+// new Player();
+// console.log(Player.getGameName());
 
-// const player = new Player();
-// player.#login
+class CompetitivePlayer extends Player {
+  rank!: number;
+  checkLogin() {
+    return this.logIn();
+  }
+
+  private isConsented() {
+    this.consent ? "Yes" : "No";
+  }
+}
+
+const player = new Player("test");
+console.log(player.connect().logIn());
+
+const player2 = new CompetitivePlayer("Test2");
+console.log(player2.connect().logIn());
+
+const somePlayer: Player | CompetitivePlayer = new CompetitivePlayer("Test3");
+somePlayer.isPro() ? console.log(somePlayer) : console.log(somePlayer);
 
 // player.login = "asdfsdfad";
 
